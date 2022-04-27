61878d355100bc00cfb5ce6373c2f3434f63f242
nouhamann <nouhamann45@gmail.com>
2022-04-27 09:56:47 +0100

    commit

---
+++ OOPinAirport/SumOf.js
@@ -0,0 +1,9 @@
+function SumOf(num1,num2){
+    return num1 + num2
+}
+
+console.log(SumOf(3,2))
+console.log(SumOf(6,9))
+console.log(SumOf(3,10))
+console.log(SumOf(7,2))
+console.log(SumOf(13.65,4.6))
